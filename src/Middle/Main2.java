389d8d63877254c3bf340d3efe0e0b1499f88f31
MishaBub <rbublik2016@gmail.com>
2026-02-12 15:30:12 +0300

    Update Main2.java

--- src/Middle/Main2.java
+++ src/Middle/Main2.java
@@ -10,14 +10,12 @@ public class Main2 {
         System.out.println("Введите число от 1 до 100: ");
         double i = scan.nextDouble();
 
-        //проверка услловия
         while (i < 1 || i > 100) {
             System.out.println("Ошибка, введите число от 1 до 100!");
             i = scan.nextDouble();
         }
 
         double[] arrayDouble = new double[]{i};
-        //создали массив и заполнили его
 
         double res = 0;
 
@@ -25,7 +23,7 @@ public class Main2 {
             res = i / o;
             if (res % 1 == 0) {
                 System.out.println(res);
-            }  // РАДУЕМСЯ ЖИЗНИ
+            }
         }
     }
 }
